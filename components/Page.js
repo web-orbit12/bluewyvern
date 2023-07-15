6ea3fe7d954d8567133dae6740b1cf0289c1eabd
web-orbit12 <goblogger125@gmail.com>
2023-07-16 02:31:25 +0400

    Fix error when page is empty

--- components/Page.js
+++ components/Page.js
@@ -1,11 +1,18 @@
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react";
 
-const Page = ({ blok }) => (
-  <main className="text-center mt-4" {...storyblokEditable(blok)}>
-    {blok.body.map((nestedBlok) => (
-      <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
-    ))}
-  </main>
-);
+const Page = ({ blok }) => {
+  // Check if blok.body exists and it is an array before mapping over it
+  if (blok && Array.isArray(blok.body)) {
+    return (
+      <main className="text-center mt-4" {...storyblokEditable(blok)}>
+        {blok.body.map((nestedBlok) => (
+          <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+        ))}
+      </main>
+    );
+  } else {
+    return null; // Or return some sort of fallback UI
+  }
+};
 
 export default Page;

f3383610e8a1cccb0540c3f4688a1b1c5b8bbc05
web-orbit12 <goblogger125@gmail.com>
2023-07-24 16:45:36 +0400

    Test embed.js - loads html css but script broken

--- public/embed.js
+++ public/embed.js
@@ -1,32 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const container = document.getElementById("nextjs-app-container");
-
-  if (container) {
-    
-    fetch('https://bluewyvern.vercel.app/')
-      .then(response => response.text())
-      .then(content => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(content, 'text/html');
-        
-        // Insert the main content
-        document.getElementById("nextjs-app-container").innerHTML = content;
-
-        // Dynamically load all script tags from the fetched content
-        doc.querySelectorAll('script').forEach(scriptTag => {
-          const script = document.createElement('script');
-          
-          if (scriptTag.src) {
-            script.src = scriptTag.src;
-          } else {
-            script.textContent = scriptTag.textContent;
-          }
-
-          // Append to the head of the document
-          document.head.appendChild(script);
+    const container = document.getElementById("nextjs-app-container");
+  
+    if (container) {
+      fetch('https://bluewyvern.vercel.app/')
+        .then(response => response.text())
+        .then(content => {
+          container.innerHTML = content;
+        })
+        .catch(error => {
+          console.error("There was an error embedding the content:", error);
         });
-      })
-      .catch(error => console.error("Error:", error));
-
-  }
-});
+    }
+  });

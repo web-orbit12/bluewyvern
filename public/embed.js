6ce54bc05819b626b67b89a23e7db2532d43f8e9
web-orbit12 <goblogger125@gmail.com>
2023-07-24 09:52:49 +0400

    Move embed.js scripts to head instead of body

--- public/embed.js
+++ public/embed.js
@@ -4,28 +4,29 @@ document.addEventListener("DOMContentLoaded", function() {
   if (container) {
     
     fetch('https://bluewyvern.vercel.app/')
-  .then(response => response.text())
-  .then(content => {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(content, 'text/html');
-    
-    // Insert the main content
-    document.getElementById("nextjs-app-container").innerHTML = content;
+      .then(response => response.text())
+      .then(content => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(content, 'text/html');
+        
+        // Insert the main content
+        document.getElementById("nextjs-app-container").innerHTML = content;
 
-    // Load all script tags
-    doc.querySelectorAll('script').forEach(scriptTag => {
-      const script = document.createElement('script');
-      
-      if (scriptTag.src) {
-        script.src = scriptTag.src;
-      } else {
-        script.textContent = scriptTag.textContent;
-      }
-      
-      document.body.appendChild(script);
-    });
-  })
-  .catch(error => console.error("Error:", error));
+        // Dynamically load all script tags from the fetched content
+        doc.querySelectorAll('script').forEach(scriptTag => {
+          const script = document.createElement('script');
+          
+          if (scriptTag.src) {
+            script.src = scriptTag.src;
+          } else {
+            script.textContent = scriptTag.textContent;
+          }
+
+          // Append to the head of the document
+          document.head.appendChild(script);
+        });
+      })
+      .catch(error => console.error("Error:", error));
 
   }
 });
